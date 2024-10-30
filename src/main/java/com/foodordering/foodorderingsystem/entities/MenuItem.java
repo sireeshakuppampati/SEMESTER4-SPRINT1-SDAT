ebb7b7ee2a2b3f22f66c2a9cf504b5341d2b5713
nayajek <ifunanya.ejeckam@keyin.com>
2024-10-30 13:39:17 -0230

    repositories of User and Order

---
+++ src/main/java/com/foodordering/foodorderingsystem/entities/MenuItem.java
@@ -0,0 +1,52 @@
+package com.foodordering.foodorderingsystem.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+}
