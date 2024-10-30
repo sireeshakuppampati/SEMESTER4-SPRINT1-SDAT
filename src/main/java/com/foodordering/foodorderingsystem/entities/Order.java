cd7cc5af27d5ea33d85579d2a7c5096fa55ba670
nayajek <ifunanya.ejeckam@keyin.com>
2024-10-30 13:39:17 -0230

    order

---
+++ src/main/java/com/foodordering/foodorderingsystem/entities/Order.java
@@ -0,0 +1,68 @@
+package com.foodordering.foodorderingsystem.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id", nullable = false)
+    private MenuItem menuItem;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    private String status;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
+has context menu
